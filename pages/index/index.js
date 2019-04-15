e6ac15be5570b445f99875d58b483116bdf37193
huzhiwu <1627983849@qq.com>
2019-04-15 23:03:51 +0800

    写了一个大图组件，一个纯文字组件

---
+++ pages/index/index.js
@@ -0,0 +1,74 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bigimg:{
+      imgSrc:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555347265438&di=14fefc944001daffa1ee9e1cf8126631&imgtype=0&src=http%3A%2F%2Fspider.nosdn.127.net%2Fc0c9b71cdd71d268190bf0737ea39c12.png",
+      smallTitle:"珍惜身边逗你笑的朋友珍惜身边逗你笑的朋友珍惜身边逗你笑的朋友珍惜身边逗你笑的朋友珍惜身边逗你笑的朋友",
+      mainTitle:'"那个逗我笑的朋友，得了抑郁症"'
+    },
+    fulltext:{
+      mainText:'"我等了三年，就是想等一个机会，我要挣一口气，不是想证明我了不起，只是要证明我失去的东西，我一定要拿回来。"',
+      headText:"香港电影的道"
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
